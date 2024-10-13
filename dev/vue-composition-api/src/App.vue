3a599e06ec874a1d52937614e3f0718dff1fd54d
evgeny <evgeny@158.160.8.209>
2024-10-13 20:57:08 +0700

    начало проекта

---
+++ dev/vue-composition-api/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import TheNav from './components/TheNav.vue'
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <TheHeader />
+    <main class="flex flex-grow flex-col"></main>
+    <TheNav />
+  </div>
+</template>
+
+<style scoped></style>
