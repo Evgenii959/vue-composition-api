b27194f367fb51bd673ef60b5710c4d8d32475c8
evgeny <evgeny@158.160.8.209>
2024-10-14 00:09:04 +0700

    добавил компоненты страниц, сделал активное меню

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
+import TheTimeline from './pages/TheTimeline.vue'
+import TheActivities from './pages/TheActivities.vue'
+import TheProgress from './pages/TheProgress.vue'
+
 </script>
 <template>
   <div class="flex min-h-screen flex-col">
     <TheHeader />
-    <main class="flex flex-grow flex-col"></main>
+    <main class="flex flex-grow flex-col">
+      <TheTimeline />
+      <TheActivities />
+      <TheProgress />
+    </main>
     <TheNav />
   </div>
 </template>
